ed380c6db5c9e02da4a8e0b9e508735733dd4bf9
GihanSVND <gihansvnd@gmail.com>
2024-09-05 23:11:17 +0530

    fix the geminiapi bug

--- TailCast/views/GenerateStory.swift
+++ TailCast/views/GenerateStory.swift
@@ -14,22 +14,54 @@ struct GenerateStory: View {
     @State var textInput = ""
     @State var fantacy:Bool = false
     @State var thriller:Bool = false
+    @State var dramatic:Bool = false
     @State var style = "normal"
     @State var wordCount = "500"
     @State var aiResponce = "Create your own story"
+    @State var generateStory:Bool = false
     
     var body: some View {
-        ScrollView{
-            ZStack {
-                
-                VStack {
+        
+        ZStack {
+            Image("GenStoryArt")
+                .resizable()
+                .frame(height: 540)
+                .offset(y:-200)
+                .ignoresSafeArea()
+            ScrollView{
+                VStack{
                     GroupBox{
                         Text(aiResponce)
                     }.padding()
+                    Spacer()
+                }
+            }
+            Button(action: {
+                generateStory = true
+            }, label: {
+                Text("Generate Story")
+            })
+            .sheet(isPresented: $generateStory, onDismiss: nil, content: {
+                VStack {
+                    HStack {
+                        Text("Enter the word Count")
+                        Spacer()
+                        TextField("Count" , text: $wordCount)
+                            .padding()
+                            .foregroundColor(.black)
+                            .textFieldStyle(.plain)
+                            .multilineTextAlignment(.trailing)
+                            .overlay(RoundedRectangle(cornerRadius: 16)
+                                .stroke(lineWidth: 3))
+                    }
                     
                     Spacer()
-                    TextField("Enter the word count" , text: $wordCount)
-                    Spacer()
+                        .frame(height: 35.0)
+                    HStack{
+                        Text("Story Style")
+                            .font(.title)
+                            .fontWeight(.bold)
+                    }
                     HStack{
                         Toggle(isOn: $fantacy, label: {
                             Text("Fantacy")
@@ -37,6 +69,7 @@ struct GenerateStory: View {
                             .onChange(of: fantacy){
                                 if fantacy == true{
                                     thriller = false
+                                    dramatic = false
                                 }
                             }
                         
@@ -46,23 +79,37 @@ struct GenerateStory: View {
                             .onChange(of: thriller){
                                 if thriller == true{
                                     fantacy = false
+                                    dramatic = false
                                 }
                             }
-                    }
-                    
-                    
+                        
+                        Toggle(isOn: $dramatic, label: {
+                            Text("Dramatic")
+                        }).toggleStyle(.button)
+                            .onChange(of: dramatic){
+                                if dramatic == true{
+                                    fantacy = false
+                                    thriller = false
+                                }
+                            }
+                        
+                        
+                    }.accentColor(.black)
                     HStack{
                         TextField("Enter the message", text: $textInput)
                         Button(action: {
                             sendMessage()
+                            generateStory = false
                         }, label: {
                             Image(systemName: "paperplane.fill")
                         })
                     }.padding()
                 }.padding()
-            }
+            })
+            
         }
     }
+    
     func sendMessage() {
         aiResponce = ""
         Task{
@@ -73,6 +120,10 @@ struct GenerateStory: View {
                 
                 if thriller == true{
                     style = "thriller"
+                }
+                
+                if dramatic == true{
+                    style = "dramatic"
                 }
                 
                 let response = try await model.generateContent("think as a creative stiry teller and writer with 20 years of experience. generate a creative and joyfull story on \"" + textInput + "\"your story shoud have simple but beautifull word manner and easy to understand. your story should be less than" + wordCount + "words. The theme of the complete story should be" + style)
