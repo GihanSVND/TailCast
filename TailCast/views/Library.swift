8d94b3489f056b9b0f5899dc4cfdbf322039d4ae
GihanSVND <gihansvnd@gmail.com>
2024-09-13 18:59:27 +0530

    update the code to download PDFs from firebase and open within the app

--- TailCast/views/Library.swift
+++ TailCast/views/Library.swift
@@ -14,37 +14,55 @@ import FirebaseAuth
 struct Library: View {
     
     @ObservedObject var model = ViewModel()
-    @State var bookCover0: UIImage?
-    @State var bookCover1: UIImage?
     @State var bookCover2: UIImage?
-    @State var bookCover3: UIImage?
+    @State var pdfUrl: URL?
     @AppStorage("uId") var userID: String = ""
     let index: Int = 1
+    @State private var showPDF = false
+    @State private var pdfURL: URL?
     
     var body: some View {
-        NavigationStack{
-            ScrollView{
-                VStack(alignment:.leading){
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading) {
                     Divider()
-                    Text("Favourits")
+                    Text("Favourites")
                         .font(.title2)
                         .fontWeight(.bold)
                     Spacer()
                         .frame(height: 40.0)
-                    HStack{
+                    
+                    HStack {
                         if model.bookList.indices.contains(index) {
                             VStack {
+                                let book = model.bookList[0]
+                                
+                                if let pdfURL = pdfURL {
+                                    NavigationLink(destination: PDFViewerView(url: pdfURL)){
+                                        Text("Edit User Profile")
+                                    }
+                                }else{
+                                    Text("Loading pdf")
+                                        .onAppear {
+                                            book.downloadBookPDF { url in
+                                                if let url = url {
+                                                    self.pdfURL = url
+                                                }
+                                            }
+                                        }
+                                }
                                 Button {
-                                    //go some where
+                                    //
+                                    
                                 } label: {
-                                    VStack(alignment:.leading) {
-                                        let book = model.bookList[0]
-                                        if let image = bookCover2{
+                                    VStack(alignment: .leading) {
+                                        let book = model.bookList[index]
+                                        if let image = bookCover2 {
+                                            // Displaying only the book cover as an image
                                             Image(uiImage: image)
                                                 .resizable()
                                                 .frame(width: 157, height: 207)
                                                 .cornerRadius(7)
-                                            
                                                 .shadow(color: .black, radius: 0, x: 5, y: 5)
                                             
                                             Text(book.Name)
@@ -58,14 +76,12 @@ struct Library: View {
                                                 .lineLimit(nil)
                                                 .font(.footnote)
                                                 .foregroundColor(Color.black)
-                                            
-                                            
-                                        }else{
+                                        } else {
                                             ProgressView()
                                                 .frame(width: 157, height: 207)
                                                 .cornerRadius(7)
-                                                .onAppear{
-                                                    book.loadBookCover{img in
+                                                .onAppear {
+                                                    book.loadBookCover { img in
                                                         self.bookCover2 = img
                                                     }
                                                 }
@@ -83,82 +99,34 @@ struct Library: View {
                                         }
                                     }
                                 }.padding()
-                            }.frame(width:160, height: 215)
+                            }.frame(width: 160, height: 215)
                         }
                     }
-                    Spacer()
-                        .frame(height: 50.0)
-                    Divider()
-                    Text("Favourits")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                    Spacer()
-                        .frame(height: 40.0)
-                    HStack{
-                        if model.bookList.indices.contains(index) {
-                            VStack {
-                                Button {
-                                    //go some where
-                                } label: {
-                                    VStack(alignment:.leading) {
-                                        let book = model.bookList[0]
-                                        if let image = bookCover2{
-                                            Image(uiImage: image)
-                                                .resizable()
-                                                .frame(width: 157, height: 207)
-                                                .cornerRadius(7)
-                                            
-                                                .shadow(color: .black, radius: 0, x: 5, y: 5)
-                                            
-                                            Text(book.Name)
-                                                .frame(width: 160)
-                                                .lineLimit(nil)
-                                                .font(.headline)
-                                                .foregroundColor(Color.black)
-                                                .padding(.top, 5.0)
-                                            Text(book.Author)
-                                                .frame(width: 160)
-                                                .lineLimit(nil)
-                                                .font(.footnote)
-                                                .foregroundColor(Color.black)
-                                            
-                                            
-                                        }else{
-                                            ProgressView()
-                                                .frame(width: 157, height: 207)
-                                                .cornerRadius(7)
-                                                .onAppear{
-                                                    book.loadBookCover{img in
-                                                        self.bookCover2 = img
-                                                    }
-                                                }
-                                            Text(book.Name)
-                                                .frame(width: 160)
-                                                .lineLimit(nil)
-                                                .font(.headline)
-                                                .foregroundColor(Color.black)
-                                                .padding(.top, 5.0)
-                                            Text(book.Author)
-                                                .frame(width: 160)
-                                                .lineLimit(nil)
-                                                .font(.footnote)
-                                                .foregroundColor(Color.black)
-                                        }
-                                    }
-                                }.padding()
-                            }.frame(width:160, height: 215)
-                        }
-                    }
-                }.navigationTitle("Library")
-                    .padding()
+                }
+                .navigationTitle("Library")
+                .padding()
+                
             }
         }
     }
-    init(){
+    
+    init() {
         model.getBookData()
+        
     }
-}
-
-#Preview {
-    Library()
+    
+    // Function to fetch the PDF from Firebase Storage
+    func fetchPDFLink(_ bookLink: String) {
+        let storageRef = Storage.storage().reference(withPath: bookLink)
+        storageRef.downloadURL { url, error in
+            if let error = error {
+                print("Error getting PDF link: \(error.localizedDescription)")
+                return
+            }
+            if let url = url {
+                self.pdfUrl = url
+                self.showPDF = true
+            }
+        }
+    }
 }
