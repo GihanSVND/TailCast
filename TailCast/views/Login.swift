ca5b77a19320a9e70ef5c1776f9d088ef81789a2
GihanSVND <gihansvnd@gmail.com>
2024-09-14 00:59:20 +0530

    finished the registerview ui

--- TailCast/views/Login.swift
+++ TailCast/views/Login.swift
@@ -54,12 +54,13 @@ struct Login: View {
                         TextField("", text: $email)
                             .foregroundColor(.black)
                             .textFieldStyle(.plain)
-                            .background(Color.white)
                             .placeholder(when: email.isEmpty){
                                 Text("Email")
                                     .foregroundColor(.black)
-                                    .bold()
+                                    
                             }
+                            .background(Color.white)
+                            
                         Spacer()
                     }.padding()
                         .background(Color.white)
@@ -79,15 +80,16 @@ struct Login: View {
                     HStack{
                         Image(systemName: "lock.fill")
                         
-                        TextField("", text: $password)
+                        SecureField("", text: $password)
                             .foregroundColor(.black)
                             .textFieldStyle(.plain)
-                            .background(Color.white)
                             .placeholder(when: password.isEmpty){
                                 Text("Password")
                                     .foregroundColor(.black)
-                                    .bold()
+                                    
                             }
+                            .background(Color.white)
+                            
                         Spacer()
                     }.padding()
                         .background(Color.white)
@@ -145,7 +147,7 @@ struct Login: View {
                 
                 
                 Button{
-                    withAnimation(Animation.bouncy){
+                    withAnimation(Animation.easeOut){
                         self.currentView = "signup"
                     }
                 } label: {
@@ -164,27 +166,3 @@ struct Login: View {
     }
 }
 
-extension String{
-    
-    func isValidEmail() -> Bool{
-        
-        let regex = try! NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: .caseInsensitive)
-
-        return regex.firstMatch(in: self, range: NSRange(location: 0, length: count)) != nil
-        
-    }
-    
-}
-
-extension String{
-    
-    func isValidPssword(_ password: String) -> Bool{
-        
-        let passwordRegex = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])(?=.*[A-Z]).{6,}$")
-        
-        return passwordRegex.evaluate(with: password)
-
-        
-    }
-    
-}
